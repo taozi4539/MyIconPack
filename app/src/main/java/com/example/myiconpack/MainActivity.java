1747b141c6af7d149b4b0ec0bdc905d062d428c5
Ailah <taozz4539@gmail.com>
2025-08-14 17:24:20 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/example/myiconpack/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.myiconpack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish(); // 打开后直接退出
+    }
+}
